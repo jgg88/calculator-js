df0e3a6b9bccf3e07fe00b0019cb07ceeb2c4956
Joshua Gomez <joshuagomez@Joshuas-MacBook-Pro.local>
2019-01-26 23:26:59 -0500

    calculator fully functioning

--- assets/logic.js
+++ assets/logic.js
@@ -1,13 +1,15 @@
 const integers = document.querySelector("#integer-body");
 const operators = document.querySelector("#operator-body");
 let result = document.querySelector("#result")
+let firstNumber;
+let secondNumber;
+let operatorType;
 
 integers.addEventListener('click', num => {
-    console.log(num.target.value)
-    let currentValue = num.target.innerText;
+    let currentValue = num.target.innerHTML;
 
     if (num.target.matches('.integer')) {
-        if (result.innerHTML === "0") {
+        if (result.innerHTML === "0" || result.innerHTML === operatorType) {
             result.innerHTML = currentValue;
         } else {
             result.append(currentValue);
@@ -16,9 +18,39 @@ integers.addEventListener('click', num => {
 
     if (num.target.matches('.clear')) {
         result.innerHTML = 0;
+        firstNumber = null;
+        secondNumber = null;
+        operatorType = null;
     }
 
+    if (firstNumber) {
+        secondNumber = parseFloat(result.innerHTML);
+    }
 
-
+    if(num.target.matches('.equals')) {
+        this.calculate();
+    }
 });
 
+operators.addEventListener('click', operator => {
+
+    //check if operatorType, incase user is using operator for second time in one calculation
+    if (operatorType) {
+        this.calculate();
+    }
+    firstNumber = parseFloat(result.innerHTML);
+    operatorType = operator.target.value;
+    result.innerHTML = operator.target.value;
+})
+
+calculate = () => {
+    if (operatorType === 'plus') {
+        result.innerHTML = firstNumber + secondNumber;
+    } else if (operatorType === 'minus') {
+        result.innerHTML = firstNumber - secondNumber;
+    } else if (operatorType === 'times') {
+        result.innerHTML = firstNumber * secondNumber;
+    } else if (operatorType === 'divided by') {
+        result.innerHTML = firstNumber / secondNumber;
+    }
+}
