0620f67aa13f3e0bf9d7e685e71012e630baef45
Joshua Gomez <joshuagomez@Joshuas-MacBook-Pro.local>
2019-01-26 20:59:09 -0500

    style clicks and  nums append to view

--- assets/logic.js
+++ assets/logic.js
@@ -1,2 +1,24 @@
-//TODO: vars for integer
-// if cases for each type of operator 
+const integers = document.querySelector("#integer-body");
+const operators = document.querySelector("#operator-body");
+let result = document.querySelector("#result")
+
+integers.addEventListener('click', num => {
+    console.log(num.target.value)
+    let currentValue = num.target.innerText;
+
+    if (num.target.matches('.integer')) {
+        if (result.innerHTML === "0") {
+            result.innerHTML = currentValue;
+        } else {
+            result.append(currentValue);
+        }
+    }
+
+    if (num.target.matches('.clear')) {
+        result.innerHTML = 0;
+    }
+
+
+
+});
+
