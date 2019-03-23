dc9510d135706bd3367edd68a425a44d6fc6801a
Joshua Gomez <gomezcreates@gmail.com>
2019-03-23 15:05:55 -0400

    added dragging functionality

--- assets/logic.js
+++ assets/logic.js
@@ -1,9 +1,23 @@
 const integers = document.querySelector("#integer-body");
 const operators = document.querySelector("#operator-body");
+
+//valuables for operations
 let result = document.querySelector("#result")
-let firstNumber;
-let secondNumber;
-let operatorType;
+let firstNumber, secondNumber, operatorType;
+
+//variables for dragging calculator
+let calculator, x, y, prevX, prevY;
+
+
+
+//TESTING HERE ========
+let el = document.getElementById('calculator'); 
+
+//   el.addEventListener("touchstart", handleStart, false);
+//   el.addEventListener("touchend", handleEnd, false);
+//   el.addEventListener("touchcancel", handleCancel, false);
+//   el.addEventListener("touchleave", handleEnd, false);
+//   el.addEventListener("touchmove", handleMove, false);
 
 integers.addEventListener('click', num => {
     let currentValue = num.target.innerHTML;
@@ -33,8 +47,7 @@ integers.addEventListener('click', num => {
 });
 
 operators.addEventListener('click', operator => {
-
-    //check if operatorType, incase user is using operator for second time in one calculation
+    //check if operatorType, in case user is using operator for second time in one calculation
     if (operatorType) {
         this.calculate();
     }
@@ -54,3 +67,42 @@ calculate = () => {
         result.innerHTML = firstNumber / secondNumber;
     }
 }
+
+drag = (e) => {
+    // console.log('here', e.target.offsetLeft)
+ calculator = e.target
+
+//  console.log(e)
+ prevX = x - calculator.offsetLeft;
+ prevY = y - calculator.offsetTop;
+}
+
+//observes mouse's position on document
+cursorPos = (e) => {
+    if (e.pageX) {
+        x = e.pageX;
+        y= e.pageY;
+    }
+    //if calculator is selected, then set to current position
+    if (calculator) {
+        console.log('in cursorPos')
+        calculator.style.left = `${x - prevX}px`;
+        calculator.style.top = `${y - prevY}px`;
+    }
+}
+
+drop = () => {
+    calculator = false;
+}
+
+document.getElementById('calculator').onmousedown = drag;
+document.onmousemove = cursorPos;
+document.onmouseup = drop;
+
+
+
+el.addEventListener("touchstart", drag, false);
+//   el.addEventListener("touchend", handleEnd, false);
+//   el.addEventListener("touchcancel", handleCancel, false);
+  el.addEventListener("touchleave", drop, false);
+  el.addEventListener("touchmove", cursorPos, false);
